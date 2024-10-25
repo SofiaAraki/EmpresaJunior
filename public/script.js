da57e92ba7cd7b455a3aaae6c0691959d02c672e
SofiaAraki <fernandasofia589@gmail.com>
2024-10-25 12:02:42 -0300

    formulario automatizado

--- public/script.js
+++ public/script.js
@@ -2,13 +2,3 @@
 
 import { analytics } from "./config/firebase-config.js";
 
-// Inicialize o Firebase Realtime Database
-const database = firebase.database();
-
-// Adicionar dados ao Realtime Database
-database.ref('Contatos').push({
-  name: Nome,
-  email: Email,
-  message: Menssagem,
-  timestamp: Date.now()
-});
